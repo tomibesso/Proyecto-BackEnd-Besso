de95c037734eb8921042665eb77e066cef9c8e29
Tomas Besso <tomi.besso12@gmail.com>
2024-04-29 23:24:29 -0300

    ordenar y agregar carpeta "src"

--- utils.js
+++
@@ -1,5 +0,0 @@
-import { fileURLToPath } from "url"; // Importa la función fileURLToPath de la biblioteca url
-import { dirname } from "path"; // Importa la función dirname de la biblioteca path
-
-const __filename = fileURLToPath(import.meta.url); // Obtiene la ruta de archivo del módulo actual
-export const __dirname = dirname(__filename); // Obtiene el nombre del directorio del archivo actual
