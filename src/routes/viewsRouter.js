62fc0c8fac459eacffc5922f811253ad221bc4fd
Tomas Besso <tomi.besso12@gmail.com>
2024-05-16 20:31:17 -0300

    agregar plantilla vista de /products

--- src/routes/viewsRouter.js
+++ src/routes/viewsRouter.js
@@ -4,7 +4,6 @@ import ProductManager from "../dao/ProductManagerMongo.js";
 const router = Router();
 
 const productsService = new ProductManager(); // Crea una instancia de ProductManager
-const products = productsService.getProducts(); // Guarda en la const los productos con el método de ProductManager
 
 const user = {
     username: "Tomibesso",
@@ -14,7 +13,9 @@ const user = {
 }
 
 // Método(petición) para mostrar el usuario y productos (no se actualiza solo)
-router.get("/home", (req, res) => {
+router.get("/home", async (req, res) => {
+    const products = await productsService.getProducts(limit = 10, numPage = 1, sortProperty = "price", sort, category, stock); // Guarda en la const los productos con el método de ProductManager
+
     res.render("home", { // Renderiza la pantilla de "home.hbs" la cual tiene los datos del usuario y los productos
         username: user.username,
         name: user.name,
@@ -27,7 +28,9 @@ router.get("/home", (req, res) => {
 })
 
 // Método(petición) para mostrar el usuario y productos (se actualiza solo con websockets)
-router.get("/realtimeproducts", (req, res) => {
+router.get("/realtimeproducts", async (req, res) => {
+    const products = await productsService.getProducts(limit = 10, numPage = 1, sortProperty = "price", sort, category, stock); // Guarda en la const los productos con el método de ProductManager
+    
     res.render("realTimeProducts", { // Renderiza la pantilla de "realTimeProducts.hbs" la cual tiene los datos del usuario y los productos
         username: user.username,
         name: user.name,
@@ -43,6 +46,15 @@ router.get("/realtimeproducts", (req, res) => {
 router.get('/chat', (req, res) => {
     res.render('chat', { // Renderiza la plantilla "chat.hbs"
         styles: './public/css/chatStyles.css' })
+})
+
+router.get('/products', async (req, res) => {
+    const products = await productsService.getProducts(); // Guarda en la const los productos con el método de ProductManager
+    console.log(products);
+
+    res.render('products', {
+        products: products.payload
+    })
 })
 
 export default router;
