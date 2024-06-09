af6bb7c7d721b7c5d17b271e469ec97d6f0a2e61
Tomas Besso <tomi.besso12@gmail.com>
2024-06-09 14:22:37 -0300

    restaurar carpeta routes

---
+++ src/routes/sessionsRouter.js
@@ -0,0 +1,122 @@
+import {Router} from 'express';
+import userManager from '../dao/UserManagerMongo.js';
+import { createHash, isValidPassword } from '../utils/bcrypt.js';
+import passport from 'passport';
+import { generateToken, authTokenMiddleware, PRIVATE_KEY } from '../utils/jsonwebtokens.js';
+import CartManager from '../dao/CartManagerMongo.js';
+
+export const sessionsRouter = Router()
+const userService = new userManager()
+const cartsService = new CartManager()
+
+sessionsRouter.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body
+
+        if(!email || !password) return res.status(401).send({status: "error", error: "Debes completar los campos obligatorios."})
+
+        const userFound = await userService.getUserBy({email})
+
+        if(!userFound) return res.status(401).send({status: "error", error: "Usuario sin credenciales"})
+
+        const isValid = isValidPassword(password, userFound.password)
+
+        if(!isValid) return res.status(401).send({status: "Error", error: "Contraseña incorrecta"})
+
+        const token = generateToken({
+            id: userFound._id,
+            email,
+            role: userFound.role
+        })
+        console.log("Inicio de sesión exitoso", "Token:" + token);
+        res.cookie("TomiCookieToken", token, {
+            maxAge: 60*60*1000*24,
+            httpOnly: true
+        }).redirect('/products')
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+sessionsRouter.post('/register', async (req, res) => {
+    try {
+        const { firstName, lastName, email, age, password } = req.body
+
+        if(!email || !password) return res.status(401).send({status: "error", error: "Debes completar los campos obligatorios."})
+    
+        const userExists = await userService.getUserBy({email})
+        if(userExists) return res.status(401).send({status:"error", error: "Usuario existente"})
+
+        const userCart = await cartsService.addCart()
+    
+        const newUser = {
+            firstName,
+            lastName,
+            email,
+            age,
+            password: createHash(password),
+            cartId: userCart._id
+        }    
+    
+        const result = await userService.addUser(newUser)
+        
+        const token = generateToken({
+            id: result._id,
+            email,
+            firstName
+        })
+        res.send({status: "success", user: result, token: token})
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+
+
+// sessionsRouter.post('/register', passport.authenticate('register', {failureRedirect: '/api/sessions/registerFail'}), async (req, res) => {
+//     res.send({status: 'success', message: 'Usuario registrado exitosamente'})
+// })
+
+// sessionsRouter.get('/registerFail', (req, res) => {
+//     console.log('Falló la estrategia de registro')
+//     res.send({error: 'Falló la estrategia de registro'})
+// })
+
+// sessionsRouter.post('/login', passport.authenticate('login', {failureRedirect: '/loginFail'}),async (req, res) => {
+//     if(!req.user) return res.status(400).send({status: 'error', error: 'credenciales invalidas'})
+//     req.session.user = {
+//         email: req.user.email,
+//         admin: req.user.role === 'admin',
+//         firstName: req.user.firstName,
+//         lastName: req.user.lastName,
+//     }
+
+//     console.log(req.session.user)
+//     res.redirect('/products')
+// })
+
+// sessionsRouter.get('/loginFail', (req, res) => {
+//     console.log('Falló la estrategia de login')
+//     res.send({error: 'Falló la estrategia de login'})
+// })
+
+sessionsRouter.get('/logout', (req, res) => {
+    res.clearCookie('TomiCookieToken').redirect('/login');
+});
+
+sessionsRouter.get('/github', passport.authenticate('github', {scope: 'user: email'}), async (req, res) => {})
+
+sessionsRouter.get('/githubcallback', passport.authenticate('github', {failureRedirect: 'localhost:8080/login'}), (req, res) => {
+    req.session.user = req.user
+    res.redirect('/products')
+})
+
+
+sessionsRouter.get('/current', passport.authenticate('current', { session: false }), (req, res) => {
+    try {
+        // Si la autenticación es exitosa, req.user contendrá la información del usuario
+        res.send({ status: "success", user: req.user });
+    } catch (error) {
+        res.status(401).send({ status: "error", message: "No está autenticado" });
+    }
+});
---
+++ src/routes/viewsRouter.js
@@ -0,0 +1,175 @@
+import { Router } from "express";
+import ProductManager from "../dao/ProductManagerMongo.js";
+import CartManager from "../dao/CartManagerMongo.js"
+import userManager from "../dao/UserManagerMongo.js";
+import { auth } from "../middlewares/authMiddleware.js";
+import { authTokenMiddleware } from "../utils/jsonwebtokens.js";
+import passport from 'passport';
+import { passportCall }  from "../utils/passportCall.js";
+import { authorization } from "../utils/authorizationJWT.js";
+
+const router = Router();
+
+const productsService = new ProductManager(); // Crea una instancia de ProductManager
+const cartService = new CartManager(); // Crea una instancia de CartManager
+const userService = new userManager(); // Crea una instancia de UserManager
+
+const user = {
+    username: "Tomibesso",
+    name: "Tomi",
+    lastName: "Besso",
+    role: "admin"
+}
+
+// Método(petición) para mostrar el usuario y productos (no se actualiza solo)
+router.get("/home", async (req, res) => {
+    const { limit, numPage, sort, category, stock} = req.query
+    const result = await productsService.getProducts(limit, numPage, "price", sort, category, stock); // Obtiene todos los productos
+
+    res.render("home", { // Renderiza la pantilla de "home.hbs" la cual tiene los datos del usuario y los productos
+        username: user.username,
+        name: user.name,
+        lastName: user.lastName,
+        role: user.role === 'admin',
+        title: 'E-Commerce Tomi Besso',
+        products: result.payload,
+        styles: "productsStyles.css"
+    })
+})
+
+// Método(petición) para mostrar el usuario y productos (se actualiza solo con websockets)
+router.get("/realtimeproducts", async (req, res) => {
+    const { limit, numPage, sort, category, stock} = req.query
+    const result = await productsService.getProducts(limit, numPage, "price", sort, category, stock); // Obtiene todos los productos
+    
+    res.render("realTimeProducts", { // Renderiza la pantilla de "realTimeProducts.hbs" la cual tiene los datos del usuario y los productos
+        username: user.username,
+        name: user.name,
+        lastName: user.lastName,
+        role: user.role === 'admin',
+        title: 'E-Commerce Tomi Besso',
+        products: result.payload,   
+        styles: "productsStyles.css",
+    })
+})
+
+// Método(petición) para mostrar el chat
+router.get('/chat', (req, res) => {
+    res.render('chat', { // Renderiza la plantilla "chat.hbs"
+        styles: './public/css/chatStyles.css' })
+})
+
+router.get('/', (req, res) => {
+    res.redirect('/products')
+})
+
+router.get('/products', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    const { limit, numPage, sort, category, stock} = req.query
+    const result = await productsService.getProducts(limit, numPage, "price", sort, category, stock); // Obtiene todos los productos
+    
+    try {       
+        const userMail = req.user.user.email
+        const user = await userService.getUserBy({ email: userMail})
+        
+        res.render('products', {
+            products: result.payload,
+            hasPrevPage: result.hasPrevPage,
+            hasNextPage: result.hasNextPage,
+            nextPage: result.nextPage,
+            prevPage: result.prevPage,
+            prevLink: result.prevLink,
+            nextLink: result.nextLink,
+            page: result.page,
+            title: "E-Commerce Tomi - Productos",
+            styles: "./public/css/productsStyles.css",
+            user: user
+        })
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+router.get('/products/:pid', async (req, res) => {
+    const { pid } = req.params
+    const result = await productsService.getProductById(pid)
+
+    res.render('productDetail', {
+        id: pid,
+        productTitle: result.title,
+        descirption: result.description,
+        price: result.price,
+        code: result.code,
+        stock: result.stock,
+        category: result.category,
+        title: "E-Commerce Tomi - Detalle"
+    }
+    )
+})
+
+router.get('/carts/:cid', async (req, res) => {
+    const { cid } = req.params
+    const result = await cartService.getCartById(cid)
+
+    if (!result) {
+        return res.status(404).send({ status: 'error', message: 'Carrito no encontrado' });
+    }
+
+    res.render('cart', {
+        cart: result.products,
+        productId: result.products._id,
+        title: "E-Commerce Tomi - Carrito",
+    })
+})
+
+router.get('/register', (req, res) => {
+    if(req.cookies['TomiCookieToken']) return res.send("Ya estas logueado");
+    res.render('register', {
+        title: "E-Commerce Tomi - Registrarse"
+    })
+})
+
+router.get('/login', (req, res) => {
+    if(req.cookies['TomiCookieToken']) return res.send("Ya estas logueado");
+    res.render('login', {
+        title: "E-Commerce Tomi - Loguearse"
+    })
+})
+
+router.get('/profile', passport.authenticate('jwt', { session: false }), async (req, res) => {
+    if (req.user) {        
+        try {
+            const email = req.user.user.email;
+            const user = await userService.getUserBy({email})
+            
+            if (!user) {
+                return res.status(404).send({ status: "error", error: "Usuario no encontrado" });
+            }
+    
+            res.render('profile', {
+                user,
+                title: "E-Commerce Tomi - Perfil"
+            });
+        } catch (error) {
+            console.error("Error al obtener perfil del usuario", error);
+        }
+    } else {
+        res.redirect('login')
+    }
+})
+
+router.get('/users', passportCall('jwt'), authorization('admin'), async (req, res) => {
+    const limit = parseInt(req.query.limit) || 10;
+    const page = parseInt(req.query.page) || 1;
+    const sort = req.query.sort || 'asc';
+    const sortProperty = req.query.sortProperty || 'lastName';
+
+    const usersData = await userService.getUsers(limit, page, sortProperty, sort); 
+    res.render('users', {
+        users: usersData,
+        title: "E-Commerce Tomi - Usuarios"
+    });
+})
+
+router.get('/current', )
+
+export default router;
